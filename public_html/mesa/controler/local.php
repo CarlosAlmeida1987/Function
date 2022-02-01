fac7903be616fb11b2c809471c191e3adac01021
CarlosAlmeida1987 <trimetria@hotmail.com>
2022-02-01 10:52:12 -0300

    Local

--- public_html/mesa/controler/local.php
+++
@@ -1,9 +0,0 @@
-<?php
- /* Dados do Banco de Dados a conectar */
-date_default_timezone_set('America/Fortaleza');
-$Servidor = 'localhost';
-$nomeBanco = 'trimetri_mesa';
-$Usuario = 'trimetri_mesa';
-$Senha = 'Grifo@1987';
-$strcon = mysqli_connect($Servidor, $Usuario, $Senha, $nomeBanco); 
-?>
