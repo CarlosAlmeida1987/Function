9e9188d9f98a70a864185a880f8ee83a6dd988b3
CarlosAlmeida1987 <trimetria@hotmail.com>
2022-02-01 11:42:57 -0300

    git ignore

--- controler/local.php
+++ controler/local.php
@@ -6,3 +6,15 @@ $Usuario = 'root';
 $Senha = '';
 $strcon = mysqli_connect($Servidor, $Usuario, $Senha, $nomeBanco); 
 ?>
+
+<?php
+echo "...";
+ /* Dados do Banco de Dados a conectar 
+date_default_timezone_set('America/Fortaleza');
+$Servidor = 'localhost';
+$nomeBanco = 'trimetri_mesa';
+$Usuario = 'trimetri_mesa';
+$Senha = 'Grifo@1987';
+$strcon = mysqli_connect($Servidor, $Usuario, $Senha, $nomeBanco);  
+*/
+?>
